1e44e5448558ed658cf82ceb6e5f36ce8f3573bb
Elcio Pereira <elcio.pereira@jbq.global>
2024-09-27 06:24:23 -0300

    feat: create migration table transactions

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,11 @@
-import { knex as setupKnex } from 'knex'
-export const config = {
+import { knex as setupKnex, Knex } from 'knex'
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: { filename: './tmp/app.db' },
   useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
 }
 export const knex = setupKnex(config)
