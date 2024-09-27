63702b93f43f0ae8763e938b251f8166230c31e4
Elcio Pereira <elcio.pereira@jbq.global>
2024-09-27 06:42:54 -0300

    feat: using enviroment variable in database.ts and create .env.example

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,14 @@
 import { knex as setupKnex, Knex } from 'knex'
+if (!process.env.DATABASE_URL) {
+  throw new Error('ENVORIMENT VARIABLE DATABASE_URL IS NOT DEFINED')
+}
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: { filename: './tmp/app.db' },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations',
+    directory: process.env.DATABASE_URL,
   },
 }
 export const knex = setupKnex(config)
